536afc3ec8685bf7698a545ac33f032aedf4d4b6
rayhankinan <rayhankinan@gmail.com>
2023-04-20 15:59:08 +0700

    Fix articulated

--- src/default-articulated.ts
+++ src/default-articulated.ts
@@ -20,51 +20,51 @@ function generateDefaultArticulated(): Articulated {
           new Draw(new Point(25, -25, 25), new Texture(1, 1)),
           new Draw(new Point(25, 25, 25), new Texture(0, 1)),
         ]),
-        // // Back Face
-        // new Face([
-        //   new Point(-25, -25, -25),
-        //   new Point(-25, 25, -25),
-        //   new Point(25, 25, -25),
-        //   new Point(-25, -25, -25),
-        //   new Point(25, 25, -25),
-        //   new Point(25, -25, -25),
-        // ]),
-        // // Top Face
-        // new Face([
-        //   new Point(-25, 25, -25),
-        //   new Point(-25, 25, 25),
-        //   new Point(25, 25, 25),
-        //   new Point(-25, 25, -25),
-        //   new Point(25, 25, 25),
-        //   new Point(25, 25, -25),
-        // ]),
-        // // Bottom face
-        // new Face([
-        //   new Point(-25, -25, -25),
-        //   new Point(25, -25, -25),
-        //   new Point(25, -25, 25),
-        //   new Point(-25, -25, -25),
-        //   new Point(25, -25, 25),
-        //   new Point(-25, -25, 25),
-        // ]),
-        // // Right face
-        // new Face([
-        //   new Point(25, -25, -25),
-        //   new Point(25, 25, -25),
-        //   new Point(25, 25, 25),
-        //   new Point(25, -25, -25),
-        //   new Point(25, 25, 25),
-        //   new Point(25, -25, 25),
-        // ]),
-        // // Left face
-        // new Face([
-        //   new Point(-25, -25, -25),
-        //   new Point(-25, -25, 25),
-        //   new Point(-25, 25, 25),
-        //   new Point(-25, -25, -25),
-        //   new Point(-25, 25, 25),
-        //   new Point(-25, 25, -25),
-        // ]),
+        // Back Face
+        new Face([
+          new Draw(new Point(-25, -25, -25), new Texture(0, 0)),
+          new Draw(new Point(-25, 25, -25), new Texture(1, 0)),
+          new Draw(new Point(25, 25, -25), new Texture(1, 1)),
+          new Draw(new Point(-25, -25, -25), new Texture(0, 0)),
+          new Draw(new Point(25, 25, -25), new Texture(1, 1)),
+          new Draw(new Point(25, -25, -25), new Texture(0, 1)),
+        ]),
+        // Top Face
+        new Face([
+          new Draw(new Point(-25, 25, -25), new Texture(0, 0)),
+          new Draw(new Point(-25, 25, 25), new Texture(1, 0)),
+          new Draw(new Point(25, 25, 25), new Texture(1, 1)),
+          new Draw(new Point(-25, 25, -25), new Texture(0, 0)),
+          new Draw(new Point(25, 25, 25), new Texture(1, 1)),
+          new Draw(new Point(25, 25, -25), new Texture(0, 1)),
+        ]),
+        // Bottom face
+        new Face([
+          new Draw(new Point(-25, -25, -25), new Texture(0, 0)),
+          new Draw(new Point(25, -25, -25), new Texture(1, 0)),
+          new Draw(new Point(25, -25, 25), new Texture(1, 1)),
+          new Draw(new Point(-25, -25, -25), new Texture(0, 0)),
+          new Draw(new Point(25, -25, 25), new Texture(1, 1)),
+          new Draw(new Point(-25, -25, 25), new Texture(0, 1)),
+        ]),
+        // Right face
+        new Face([
+          new Draw(new Point(25, -25, -25), new Texture(0, 0)),
+          new Draw(new Point(25, 25, -25), new Texture(1, 0)),
+          new Draw(new Point(25, 25, 25), new Texture(1, 1)),
+          new Draw(new Point(25, -25, -25), new Texture(0, 0)),
+          new Draw(new Point(25, 25, 25), new Texture(1, 1)),
+          new Draw(new Point(25, -25, 25), new Texture(0, 1)),
+        ]),
+        // Left face
+        new Face([
+          new Draw(new Point(-25, -25, -25), new Texture(0, 0)),
+          new Draw(new Point(-25, -25, 25), new Texture(1, 0)),
+          new Draw(new Point(-25, 25, 25), new Texture(1, 1)),
+          new Draw(new Point(-25, -25, -25), new Texture(0, 0)),
+          new Draw(new Point(-25, 25, 25), new Texture(1, 1)),
+          new Draw(new Point(-25, 25, -25), new Texture(0, 1)),
+        ]),
       ],
       0,
       0,
