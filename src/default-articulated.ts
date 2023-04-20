85264d08e45160dc15c06cd56ad080b3add7e1ba
rayhankinan <rayhankinan@gmail.com>
2023-04-20 14:50:36 +0700

    Change default to square

--- src/default-articulated.ts
+++ src/default-articulated.ts
@@ -1,10 +1,79 @@
 import Articulated from "Objects/articulated";
 import Node from "Objects/node";
 import Face from "Objects/face";
+import Point from "Objects/point";
 
 function generateDefaultArticulated(): Articulated {
   return new Articulated(
-    new Node("root", [], [new Face([])], 0, 0, 0, 0, 0, 0, 1, 1, 1)
+    new Node(
+      "root",
+      [],
+      [
+        // Front Face
+        new Face([
+          new Point(-25, 25, 25),
+          new Point(-25, -25, 25),
+          new Point(25, -25, 25),
+          new Point(-25, 25, 25),
+          new Point(25, -25, 25),
+          new Point(25, 25, 25),
+        ]),
+        // Back Face
+        new Face([
+          new Point(-25, -25, -25),
+          new Point(-25, 25, -25),
+          new Point(25, 25, -25),
+          new Point(-25, -25, -25),
+          new Point(25, 25, -25),
+          new Point(25, -25, -25),
+        ]),
+        // Top Face
+        new Face([
+          new Point(-25, 25, -25),
+          new Point(-25, 25, 25),
+          new Point(25, 25, 25),
+          new Point(-25, 25, -25),
+          new Point(25, 25, 25),
+          new Point(25, 25, -25),
+        ]),
+        // Bottom face
+        new Face([
+          new Point(-25, -25, -25),
+          new Point(25, -25, -25),
+          new Point(25, -25, 25),
+          new Point(-25, -25, -25),
+          new Point(25, -25, 25),
+          new Point(-25, -25, 25),
+        ]),
+        // Right face
+        new Face([
+          new Point(25, -25, -25),
+          new Point(25, 25, -25),
+          new Point(25, 25, 25),
+          new Point(25, -25, -25),
+          new Point(25, 25, 25),
+          new Point(25, -25, 25),
+        ]),
+        // Left face
+        new Face([
+          new Point(-25, -25, -25),
+          new Point(-25, -25, 25),
+          new Point(-25, 25, 25),
+          new Point(-25, -25, -25),
+          new Point(-25, 25, 25),
+          new Point(-25, 25, -25),
+        ]),
+      ],
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      1,
+      1,
+      1
+    )
   );
 }
 
